b1fef3c5917122d5abf7f825eed3dcfb6a70f239
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-10-06 23:23:04 -0500

    Duplicated code and unused widget

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -11,6 +11,12 @@ class cart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var itemNameStyle = Theme.of(context).textTheme.titleLarge;
+    // This gets the current state of CartModel and also tells Flutter
+    // to rebuild this widget when CartModel notifies listeners (in other words,
+    // when it changes).
+    var cart = context.watch<cartmodel>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart', style: Theme.of(context).textTheme.displayLarge),
@@ -23,7 +29,22 @@ class cart extends StatelessWidget {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(32),
-                child: cartlist(),
+                child: ListView.builder(
+                  itemCount: cart.items.length,
+                  itemBuilder: (context, index) => ListTile(
+                    leading: const Icon(Icons.done),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.remove_circle_outline),
+                      onPressed: () {
+                        cart.remove(cart.items[index]);
+                      },
+                    ),
+                    title: Text(
+                      cart.items[index].name,
+                      style: itemNameStyle,
+                    ),
+                  ),
+                ),
               ),
             ),
             const Divider(height: 4, color: Colors.black),
